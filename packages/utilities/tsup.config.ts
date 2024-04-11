fe949347625bee1a891d424ea201cea62c327ef3
Ryan Howard <rollercodester@live.com>
2024-04-11 11:03:24 -0500

    Add tsup exclusion for test files

--- packages/utilities/tsup.config.ts
+++ packages/utilities/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['src/**/*@(ts|tsx)'],
+  entry: ['src/**/*@(ts|tsx)', '!src/**/*.test.@(ts|tsx)'],
   format: ['esm', 'cjs'],
   minify: true,
   outDir: 'dist',
